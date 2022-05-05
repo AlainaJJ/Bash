6b6577f551cd1c1b36c08b18d8dc969d2bcf23d2
AlainaJJ <ajjimenez@ucsd.edu>
2022-05-05 10:25:37 -0700

    Update newRun.sh

--- newRun.sh
+++ newRun.sh
@@ -1,11 +1,11 @@
-if [ $# -eq 0 ]
-then
-  echo "No argument is provided"
-  exit 1
-fi
+#if [ $# -eq 0 ]
+#then
+#  echo "No argument is provided"
+#  exit 1
+#fi
 
-ANIMAL=$1
-printf "Your favorite animal is %s\n" $ANIMAL
+#ANIMAL=$1
+#printf "Your favorite animal is %s\n" $ANIMAL
 
 i=8
 
